5f37d6edc41e2971327e4cca052dfbb0e82899ae
Steven <steven@bitcards.com>
2025-09-01 11:26:52 -0700

    Adding routes to main.py and trying to fix issue creation through zap

--- app/src/main.py
+++ app/src/main.py
@@ -1,11 +1,38 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return ""
 
 @app.route("/health")
 def health():
     return "ok"
 
+@app.route("/robots.txt")
+def robots_txt():
+    return ""
+
+@app.route("/sitemap.xml")
+def sitemap_xml():
+    xml_content = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+   <url>
+      <loc>http://127.0.0.1:8000/health</loc>
+      <lastmod>2025-09-01</lastmod>
+      <changefreq>never</changefreq>
+      <priority>0.0</priority>
+   </url>
+   <url>
+      <loc>http://127.0.0.1:8000/</loc>
+      <lastmod>2025-09-01</lastmod>
+      <changefreq>never</changefreq>
+      <priority>0.0</priority>
+   </url>
+</urlset>"""
+
+    return Response(xml_content, mimetype='text/xml')
 @app.route("/hello")
 def hello():
     name = request.args.get("name", "world")
